acd25b418774327e38aca32dfd17615fa7438062
Artem Rogov <rogar@omegafuture.ru>
2026-01-13 19:43:30 +0300

    test simple sso component js

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname));
 
 // Запускаем сервер
 app.listen(port, () => {
-  console.log(`Сервер запущен! Доступен по адресу http://localhost:${port}/`);
+  console.log(`Сервер запущен! Доступен по адресу http://localhost:${port}/index.html`);
 });
